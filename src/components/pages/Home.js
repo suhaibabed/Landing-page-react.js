9ce5af64dbb2fba96f2d35b5d5ac400db8b2eb57
sohib abed <sohibabed13@gmail.com>
2021-10-19 23:42:07 +0300

    fisr-commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import "../../App.css";
+import HeroSection from "../HeroSection/HeroSection";
+import Cards from '../card/Cards'
+import Footer from '../Footer/Footer'
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
